e32ee4640bf4ad5b625efb6a1c5af4bffc554337
“Numan-star” <5125.2019.gct@gmail.com>
2024-09-05 13:02:06 +0500

    change design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import Header from './components/Header'; 
-import Footer from './components/Footer'; 
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default function Home() {
   return (
-    <main className="flex justify-center lg:items-center md:items-center bg-black min-h-screen">
-      <div className="card-container rounded max-w-md w-full mx-auto text-white lg:max-h-screen lg:overflow-auto">
+    // <main className="flex justify-center lg:items-center md:items-center bg-black min-h-screen">
+    //   <div className="card-container rounded max-w-md w-full mx-auto text-white lg:max-h-screen lg:overflow-auto">
+
+    <main className="">
+      <div className="">
         <Header />
         <Footer />
       </div>
